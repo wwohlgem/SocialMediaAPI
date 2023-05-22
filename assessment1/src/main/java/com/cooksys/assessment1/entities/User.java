27881238f151e9379bad61382a473287590a517c
Ricardo Lopez <102694854+ricalope@users.noreply.github.com>
2023-05-22 15:56:16 -0700

    fixed merge issue in User file

--- assessment1/src/main/java/com/cooksys/assessment1/entities/User.java
+++ assessment1/src/main/java/com/cooksys/assessment1/entities/User.java
@@ -2,27 +2,21 @@ package com.cooksys.assessment1.entities;
 
 import java.time.LocalDateTime;
 
-<<<<<<< HEAD
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-=======
-import jakarta.persistence.*;
->>>>>>> 65a9b34619d64fb3b5061a4284042ef3a383ecc1
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @NoArgsConstructor
-<<<<<<< HEAD
 @Table(name="user_table")
-=======
-@Table(name = "user_table")
->>>>>>> 65a9b34619d64fb3b5061a4284042ef3a383ecc1
 public class User {
 
 	@Id
