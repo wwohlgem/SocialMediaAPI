65a9b34619d64fb3b5061a4284042ef3a383ecc1
Wendelin Wohlgemuth <wendelinwohlgemuth@Wendelins-MacBook-Pro.local>
2023-05-22 15:34:16 -0700

    created ContextDto

--- assessment1/src/main/java/com/cooksys/assessment1/entities/User.java
+++ assessment1/src/main/java/com/cooksys/assessment1/entities/User.java
@@ -2,17 +2,14 @@ package com.cooksys.assessment1.entities;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Embedded;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @NoArgsConstructor
+@Table(name = "user_table")
 public class User {
 
 	@Id
---
+++ assessment1/src/main/java/com/cooksys/assessment1/model/ContextDto.java
@@ -0,0 +1,19 @@
+package com.cooksys.assessment1.model;
+
+import com.cooksys.assessment1.entities.Tweet;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ContextDto {
+
+    private Tweet target;
+    private List<Tweet> before;
+    private List<Tweet> after;
+
+}
